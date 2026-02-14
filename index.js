e7e25c9e71c9627801f10813676b00d92f156291
sezo45 <wayaslam567@gmail.com>
2026-02-14 21:39:34 +0200

    vertion11

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ window.addEventListener("scroll", function () {
     header.classList.remove("scrolled");
   }
 });
+// _________________________________
+document.addEventListener("DOMContentLoaded", function () {
+  const backToTopBtn = document.getElementById("backToTop");
+  window.onscroll = function () {
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+      backToTopBtn.style.display = "flex";
+    } else {
+      backToTopBtn.style.display = "none";
+    }
+  };
+  backToTopBtn.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+});
